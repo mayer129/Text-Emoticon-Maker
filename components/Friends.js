d0514e25a584cb7afd2aebe86bacac5c9233a1e5
mayer129 <mayer129@mail.chapman.edu>
2021-12-16 15:02:49 -0800

    testing initial friends.js

---
+++ components/Friends.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import { supabase } from "../utils/supabaseClient"
+
+export default function Friends() {
+    const[friendFavorites, setFriendFavorites] = useState([])
+
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+
+    let submitForm = e => {
+        e.preventDefault()
+        retreiveFriend(query)
+    }
+
+    const retreiveFriend = async (username) => {
+        const { data, error } = await supabase
+
+    }
+}
